fc8efb74f344b59743943b8a1349e50b66712c2c
inbeomi <inbeom0907@gmail.com>
2022-03-26 12:06:20 +0000

    Refactor : main_OOOO 함수 리팩토링

--- dataset/main_dataset.py
+++ dataset/main_dataset.py
@@ -1,1 +1,74 @@
-from dataset import *
+import sys
+import pickle
+sys.path.append('..')
+import pandas as pd
+import torch
+from preprocess.main_preprocess import *
+
+# from dataset import *
+
+
+class RE_Dataset(torch.utils.data.Dataset):
+    """ 
+    Dataset 구성을 위한 class.
+    """
+    def __init__(self, pair_dataset, labels):
+        self.pair_dataset = pair_dataset
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: val[idx].clone().detach() for key, val in self.pair_dataset.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+
+def load_data(dataset_dir):
+    """ 
+    csv 파일을 경로에 맡게 불러 옵니다.
+     """
+    pd_dataset = pd.read_csv(dataset_dir)
+    dataset = preprocessing_dataset(pd_dataset)
+  
+    return dataset
+
+
+def load_test_dataset(dataset_dir, tokenizer):
+    """
+    test dataset을 불러온 후,
+    tokenizing 합니다.
+    """
+    test_dataset = load_data(dataset_dir)
+    test_label = list(map(int,test_dataset['label'].values))
+    # tokenizing dataset
+    tokenized_test = tokenized_dataset(test_dataset, tokenizer)
+
+    return test_dataset['id'], tokenized_test, test_label
+
+
+def label_to_num(label):
+    """
+    
+    """
+    num_label = []
+    with open('dict_label_to_num.pkl', 'rb') as f:
+        dict_label_to_num = pickle.load(f)
+    for v in label:
+        num_label.append(dict_label_to_num[v])
+
+    return num_label
+
+
+def num_to_label(label):
+    """
+    숫자로 되어 있던 class를 원본 문자열 라벨로 변환 합니다.
+    """
+    origin_label = []
+    with open('dict_num_to_label.pkl', 'rb') as f:
+        dict_num_to_label = pickle.load(f)
+    for v in label:
+        origin_label.append(dict_num_to_label[v])
+
+    return origin_label
