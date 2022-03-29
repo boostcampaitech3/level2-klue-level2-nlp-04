03d7775d983dd353c4d81acf5a62992949985972
hyoeun <f2921641@naver.com>
2022-03-29 06:30:00 +0000

    Fix : training_args를 argparse에서 받아 오기 추가

--- train.py
+++ train.py
@@ -80,14 +80,14 @@ def label_to_num(label):
     return num_label
 
 
-def train():
+def train(args):
     # load model and tokenizer
     # MODEL_NAME = "bert-base-uncased"
-    MODEL_NAME = "klue/bert-base"
+    MODEL_NAME = args.model
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
     # load dataset
-    train_dataset = load_data("../dataset/train/train.csv")
+    train_dataset = load_data(args.train_path)
     # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
 
     train_label = label_to_num(train_dataset['label'].values)
@@ -116,22 +116,22 @@ def train():
     # 사용한 option 외에도 다양한 option들이 있습니다.
     # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
     training_args = TrainingArguments(
-      output_dir='./results',          # output directory
+      output_dir=args.save_dir,          # output directory
       save_total_limit=5,              # number of total save model.
-      save_steps=500,                 # model saving step.
-      num_train_epochs=20,              # total number of training epochs
-      learning_rate=5e-5,               # learning_rate
-      per_device_train_batch_size=16,  # batch size per device during training
-      per_device_eval_batch_size=16,   # batch size for evaluation
+      save_steps=args.save_steps,                 # model saving step.
+      num_train_epochs=args.epochs,              # total number of training epochs
+      learning_rate=args.lr,               # learning_rate
+      per_device_train_batch_size=args.batch,  # batch size per device during training
+      per_device_eval_batch_size=args.batch_valid,   # batch size for evaluation
       warmup_steps=500,                # number of warmup steps for learning rate scheduler
-      weight_decay=0.01,               # strength of weight decay
+      weight_decay=args.warmup,               # strength of weight decay
       logging_dir='./logs',            # directory for storing logs
-      logging_steps=100,              # log saving step.
+      logging_steps=args.logging_steps,              # log saving step.
       evaluation_strategy='steps', # evaluation strategy to adopt during training
                                   # `no`: No evaluation during training.
                                   # `steps`: Evaluate every `eval_steps`.
                                   # `epoch`: Evaluate every end of epoch.
-      eval_steps = 500,            # evaluation step.
+      eval_steps = args.eval_steps,            # evaluation step.
       load_best_model_at_end = True,
       report_to='wandb' 
     )
@@ -154,8 +154,8 @@ def main():
     """path, model option"""
     parser.add_argument('--seed', type=int, default=42,
                         help='random seed (default: 42)')
-    parser.add_argument('--save_dir', type=str, default = './best_model', 
-                        help='model save dir path (default : ./best_model)')
+    parser.add_argument('--save_dir', type=str, default = './results', 
+                        help='model save dir path (default : ./results)')
     parser.add_argument('--wandb_path', type= str, default= 'test-project',
                         help='wandb graph, save_dir basic path (default: test-project') 
     parser.add_argument('--train_path', type= str, default= '/opt/ml/dataset/train/train.csv',
@@ -173,24 +173,24 @@ def main():
 
 
     """hyperparameter"""
-    parser.add_argument('--epochs', type=int, default=5,
-                        help='number of epochs to train (default: 5)')
-    parser.add_argument('--lr', type=float, default=1e-5,
-                        help='learning rate (default: 1e-5)')
-    parser.add_argument('--batch', type=int, default=32,
-                        help='input batch size for training (default: 32)')
+    parser.add_argument('--epochs', type=int, default=20,
+                        help='number of epochs to train (default: 20)')
+    parser.add_argument('--lr', type=float, default=5e-5,
+                        help='learning rate (default: 5e-5)')
+    parser.add_argument('--batch', type=int, default=16,
+                        help='input batch size for training (default: 16)')
     parser.add_argument('--gradient_accum', type=int, default=2,
                         help='gradient accumulation (default: 2)')
-    parser.add_argument('--batch_valid', type=int, default=32,
-                        help='input batch size for validing (default: 32)')
+    parser.add_argument('--batch_valid', type=int, default=16,
+                        help='input batch size for validing (default: 16)')
     parser.add_argument('--warmup', type=int, default=0.1,
                         help='warmup_ratio (default: 0.1)')
-    parser.add_argument('--eval_steps', type=int, default=250,
-                        help='eval_steps (default: 250)')
-    parser.add_argument('--save_steps', type=int, default=250,
-                        help='save_steps (default: 250)')
+    parser.add_argument('--eval_steps', type=int, default=500,
+                        help='eval_steps (default: 500)')
+    parser.add_argument('--save_steps', type=int, default=500,
+                        help='save_steps (default: 500)')
     parser.add_argument('--logging_steps', type=int,
-                        default=50, help='logging_steps (default: 50)')
+                        default=100, help='logging_steps (default: 100)')
     parser.add_argument('--weight_decay', type=float,
                         default=0.01, help='weight_decay (default: 0.01)')
     parser.add_argument('--metric_for_best_model', type=str, default='micro f1 score',
@@ -198,7 +198,7 @@ def main():
     
     args= parser.parse_args()
     wandb.init(project="test-project", entity="boostcamp_nlp_04", config = vars(args),)
-    train()
+    train(args)
 
 
 if __name__ == '__main__':
