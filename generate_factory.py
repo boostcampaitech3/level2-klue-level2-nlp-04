c8883a1dccea5be327767e14322d8fdf9c3f25f7
inbeomi <inbeom0907@gmail.com>
2022-04-06 08:46:42 +0000

    Fix: tqdm, print문 수정

--- generate_factory.py
+++ generate_factory.py
@@ -2,7 +2,7 @@ import time
 import torch
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from torch.cuda.amp import autocast
 
 tokenizer = AutoTokenizer.from_pretrained('kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',
@@ -12,16 +12,18 @@ model = AutoModelForCausalLM.from_pretrained('kakaobrain/kogpt', revision='KoGPT
 
 _ = model.eval()
 
-def generate(df:pd.DataFrame, SAVE_DIR:str)->pd.DataFrame:
+def generate(df:pd.DataFrame, SAVE_DIR:str)->None:
+    print("화이팅!!!!!!!")
     for i in tqdm(range(len(df))):
         prompt = df['sentence'][i]
         with torch.no_grad():
+            tokens = tokenizer.encode(prompt, return_tensors='pt').to(device='cuda', non_blocking=True)
             with torch.cuda.amp.autocast():
-                tokens = tokenizer.encode(prompt, return_tensors='pt').to(device='cuda', non_blocking=True)
                 gen_tokens = model.generate(tokens ,do_sample=True, temperature=0.8, max_length = 64)
-                generated = tokenizer.batch_decode(gen_tokens)[0]
+            generated = tokenizer.batch_decode(gen_tokens)[0]
         df['sentence'][i] = generated
     df.to_csv(SAVE_DIR, encoding = 'utf-8')
+    print("종료되었습니다.")
 
 if __name__ == '__main__':
 
